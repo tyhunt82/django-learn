541a403abfe9d365b8c8ddd5271f083f775c1785
Tyler Hunt <tyhunt82@users.noreply.github.com>
2024-04-28 21:39:02 +0000

    init project

---
+++ members/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Member(models.Model):
+  firstname = models.CharField(max_length=255)
+  lastname = models.CharField(max_length=255)
+  email = models.CharField(max_length=255)
